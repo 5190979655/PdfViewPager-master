2b4629858bb032bc767d0b1379194f791276a4a3
unknown <519097965@qq.com>
2018-04-10 08:54:09 +0800

    first commit

---
+++ library/src/main/java/es/voghdev/pdfviewpager/library/PDFViewPager2.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2016 Olmo Gallegos Hernández.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.voghdev.pdfviewpager.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter;
+
+public class PDFViewPager2 extends ViewPager {
+    protected Context context;
+    private PDFPagerAdapter pdfPagerAdapter;
+
+    public PDFViewPager2(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    public PDFViewPager2(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        init(attrs);
+    }
+
+    public void init(String pdfPath) {
+        initAdapter(context, pdfPath);
+    }
+
+    protected void init(AttributeSet attrs) {
+        if (isInEditMode()) {
+            setBackgroundResource(R.drawable.flaticon_pdf_dummy);
+            return;
+        }
+
+        if (attrs != null) {
+            TypedArray a;
+
+            a = context.obtainStyledAttributes(attrs, R.styleable.PDFViewPager);
+            String assetFileName = a.getString(R.styleable.PDFViewPager_assetFileName);
+
+            if (assetFileName != null && assetFileName.length() > 0) {
+                initAdapter(context, assetFileName);
+            }
+
+            a.recycle();
+        }
+    }
+
+    protected void initAdapter(Context context, String pdfPath) {
+        pdfPagerAdapter = new PDFPagerAdapter.Builder(context)
+                .setPdfPath(pdfPath)
+                .setOffScreenSize(getOffscreenPageLimit())
+                .create();
+        setAdapter(pdfPagerAdapter);
+    }
+
+    public int count(){
+        if (pdfPagerAdapter!=null)
+            return pdfPagerAdapter.getCount();
+        return 0;
+    }
+
+    /**
+     * PDFViewPager uses PhotoView, so this bugfix should be added
+     * Issue explained in https://github.com/chrisbanes/PhotoView
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
